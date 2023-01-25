c603ea554dadabdf107eaae060c918286163c614
imakhlaq <imakhlaqXD@gmail.com>
2023-01-25 20:40:04 +0530

    speed and size values

--- src/store/ContextProvider.tsx
+++ src/store/ContextProvider.tsx
@@ -7,10 +7,16 @@ type contextProviderProps = {
 
 const ContextProvider = ({ children }: contextProviderProps) => {
   const [selectedAlgo, setSelectedAlgo] = useState("Slection Sort");
+  const [speed,setSpeed]= useState(400);
+  const [size,setSize]=useState(25);
 
   const value = {
     selectedAlgo,
     setSelectedAlgo,
+    speed,
+    setSpeed,
+    size,
+    setSize
   };
 
   return <MyContext.Provider value={value}>{children}</MyContext.Provider>;
--- src/store/MyContext.ts
+++ src/store/MyContext.ts
@@ -1,9 +1,13 @@
 import { createContext } from "react";
 
-type MyContextType={
-  selectedAlgo:string
-  setSelectedAlgo:(algo:string)=>void,
-}
+type MyContextType = {
+  selectedAlgo: string;
+  setSelectedAlgo: (algo: string) => void;
+  speed: number;
+  setSpeed: (speed: number) => void;
+  size: number;
+  setSize: (speed: number) => void;
+};
 
 const MyContext = createContext({} as MyContextType);
 
