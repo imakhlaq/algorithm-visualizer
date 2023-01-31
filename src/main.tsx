83fcb9fa779a55f59a5d7f2a113b680c609042dc
imakhlaq <imakhlaqXD@gmail.com>
2023-01-31 21:31:09 +0530

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import ContextProvider from "./store/contextProvider";
+import ContextProvider from "./store/ContextProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
